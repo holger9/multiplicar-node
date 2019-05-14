c155f92fc7794d116eba0342ca2858e86bb82634
Holger <holgermurillo9@gmail.com>
2019-05-13 23:37:04 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,49 @@
+//requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+  console.log('====================================='.green);
+  console.log(`=====Tabla del ${ base }==============`.green);
+  console.log('====================================='.green);
+  for(let i = 1; i <= limite; i++){
+    console.log(`${ base } * ${ i } = ${ base * i }`);
+  }
+}
+
+let crearArchivo = (base, limite = 10) => {
+  return new Promise((resolve, reject) => {
+    if(!Number(base)){
+      reject(`El valor introducido ${ base } no es un número`);
+      return //funciona como break
+    }
+    let data = '';
+
+    for(let i = 1; i <= limite; i++){
+      //console.log(`${ base } * ${ i } = ${ base * i }`);
+      data += `${ base } * ${ i } = ${ base * i }\n`
+    }
+    //crear archivo
+    fs.writeFile(`archivos/tabla-${ base }-al-${ limite }.txt`, data, (err) => {
+      if(err)
+        reject(err);
+      else
+        resolve(`tabla-${ base }-al-${ limite }.txt`);
+    });
+
+  });
+}
+
+/*
+fs.writeFile('tabla-2.txt', 'hola mundo', (err) => {
+  if(err) throw err;
+  console.log('El archivo tabla-2.txt ha sido creado');
+});
+*/
+
+
+module.exports = {
+  crearArchivo,
+  listarTabla
+}
